aa78a0146b87fbf5ab52dc18beb52abc1325d1f2
Dipak Swain <58125288+aboutdeepak4567@users.noreply.github.com>
2026-09-09 00:46:54 +0530

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 1;
+        int count = 0;
+        for(int num : nums){
+            if(count == 0){
+                res = num;
+            }
+            if(num == res){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return res;
+
+        
+    }
+}
